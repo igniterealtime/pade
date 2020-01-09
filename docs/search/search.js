aeb77d6e7b673a86b69bb7c7d20e8d9a5265514f
Dele Olajide <dele@4ng.net>
2020-01-09 15:18:48 +0000

    added participants to search

--- docs/search/search.js
+++ docs/search/search.js
@@ -16,22 +16,48 @@
             _converse = this._converse;
             dayjs = converse.env.dayjs;
 
+            _converse.api.settings.update({
+                search_max: 999,
+                search_free_text_search: true
+            });
+
+
             SearchDialog = _converse.BootstrapModal.extend({
                 initialize() {
                     _converse.BootstrapModal.prototype.initialize.apply(this, arguments);
                     this.model.on('change', this.render, this);
                 },
                 toHTML() {
-                  return '<div class="modal" id="myModal"> <div class="modal-dialog modal-lg"> <div class="modal-content">' +
+                    const view = this.model.get("view");
+                    const jid = view.model.get("jid");
+                    const type = view.model.get("type");
+                    const label = view.model.getDisplayName();
+
+                    let participants = '';
+                    participants = participants + '   <label for="pade-search-participants"></label>';
+                    participants = participants + '   <select class="form-control" id="pade-search-participants" name="pade-search-participants">';
+                    participants = participants + '       <option value="' + jid + '">' + label + '</option>';
+
+                    if (type == _converse.CHATROOMS_TYPE)
+                    {
+                        view.model.occupants.each(function (occupant) {
+                            if (occupant.get("jid")) participants = participants + '       <option value="' + occupant.get("jid") + '">' + occupant.get("nick") + '</option>';
+                        });
+                    }
+                    participants = participants + '   </select>';
+
+                    return '<div class="modal" id="myModal"> <div class="modal-dialog modal-lg"> <div class="modal-content">' +
                          '<div class="modal-header"><h1 class="modal-title">Search</h1><button type="button" class="close" data-dismiss="modal">&times;</button></div>' +
                          '<div class="modal-body">' +
-                         '<input id="pade-search-keywords" class="form-control" type="text" placeholder="Type a query and press [Enter] to search" ><p/><div id="pade-search-results"></div>' +
+                         '<input id="pade-search-keywords" class="form-control" type="text" placeholder="Type a query and press [Enter] to search" >' +
+                         participants +
+                         '<p/><div id="pade-search-results"></div>' +
                          '</div>' +
                          '<div class="modal-footer"> <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button> </div>' +
                          '</div> </div> </div>';
                 },
                 afterRender() {
-                  var that = this;
+                  const that = this;
                   this.el.addEventListener('shown.bs.modal', function()
                   {
                       if (that.model.get("keyword"))
@@ -52,57 +78,57 @@
                 keyUp(ev) {
                     if (ev.key === "Enter")
                     {
-                        var keyword = this.el.querySelector("#pade-search-keywords").value.trim();
+                        const keyword = this.el.querySelector("#pade-search-keywords").value.trim();
+                        const participant = this.el.querySelector("#pade-search-participants").value.trim();
+
                         this.model.set("keyword", keyword)
+                        this.model.set("participant", participant)
                         this.doSearch();
                     }
                 },
 
                 doSearch() {
-                    var view = this.model.get("view");
-                    var jid = view.model.get("jid");
-                    var type = view.model.get("type");
+                    const view = this.model.get("view");
+                    const type = view.model.get("type");
 
-                    var that = this;
-                    var keyword = that.model.get("keyword");
-                    var searchRegExp = new RegExp('^(.*)(\s?' + keyword + ')', 'i');
-                    var tagRegExp = new RegExp("(\\b" + keyword + "\\b)", "im");
+                    const that = this;
+                    const keyword = this.model.get("keyword");
+                    const participant = this.model.get("participant");
+                    const searchRegExp = new RegExp('^(.*)(\s?' + keyword + ')', 'i');
+                    const tagRegExp = new RegExp("(\\b" + keyword + "\\b)", "im");
 
-                    console.debug("doSearch", keyword, jid);
+                    console.debug("doSearch", keyword, participant);
 
-                    if (keyword != "")
+                    const searchResults = that.el.querySelector("#pade-search-results");
+                    searchResults.innerHTML = "No Match";
+
+                    _converse.api.archive.query({before: '', search: keyword, max: _converse.api.settings.get("search_max"), 'groupchat': true, 'with': participant}).then(function(result)
                     {
-                        var searchResults = that.el.querySelector("#pade-search-results");
-                        searchResults.innerHTML = "No Match";
+                        const messages = result.messages;
+                        let html = "<table style='margin-left: 15px'><tr><th>Date</th><th>Message</th><th>Participant</th></tr>";
 
-                        _converse.api.archive.query({before: '', max: 999, 'groupchat': true, 'with': jid}).then(function(result)
+                        for (let i=0; i<messages.length; i++)
                         {
-                            const messages = result.messages;
-                            var html = "<table style='margin-left: 15px'><tr><th>Date</th><th>Message</th><th>Participant</th></tr>";
-
-                            for (var i=0; i<messages.length; i++)
+                            if (messages[i].querySelector('body'))
                             {
-                                if (messages[i].querySelector('body'))
-                                {
-                                    var body = messages[i].querySelector('body').innerHTML;
-                                    var delay = messages[i].querySelector('forwarded').querySelector('delay');
-                                    var from = messages[i].querySelector('forwarded').querySelector('message').getAttribute('from');
-                                    var time = delay ? delay.getAttribute('stamp') : dayjs().format();
-                                    var pretty_time = dayjs(time).format('MMM DD<br/>HH:mm:ss');
-                                    var pretty_from = type === "chatroom" ? from.split("/")[1] : from.split("@")[0];
+                                const body = messages[i].querySelector('body').innerHTML;
+                                const delay = messages[i].querySelector('forwarded').querySelector('delay');
+                                const from = messages[i].querySelector('forwarded').querySelector('message').getAttribute('from');
+                                const time = delay ? delay.getAttribute('stamp') : dayjs().format();
+                                const pretty_time = dayjs(time).format('MMM DD<br/>HH:mm:ss');
+                                const pretty_from = type === "chatroom" ? from.split("/")[1] : from.split("@")[0];
 
-                                    if (searchRegExp.test(body))
-                                    {
-                                        var tagged = body.replace(tagRegExp, "<span style=background-color:#FF9;color:#555;>$1</span>");
-                                        html = html + "<tr><td>" + pretty_time + "</td><td>" + tagged + "</td><td>" + pretty_from + "</td></tr>";
-                                    }
+                                if (keyword == "" || searchRegExp.test(body))
+                                {
+                                    const tagged = body.replace(tagRegExp, "<span style=background-color:#FF9;color:#555;>$1</span>");
+                                    html = html + "<tr><td>" + pretty_time + "</td><td>" + tagged + "</td><td>" + pretty_from + "</td></tr>";
                                 }
                             }
+                        }
 
-                            html =  html + "</table>";
-                            searchResults.innerHTML = html;
-                        });
-                    }
+                        html =  html + "</table>";
+                        searchResults.innerHTML = html;
+                    });
                 }
             });
 
@@ -147,7 +173,7 @@
 
     function newElement (el, id, html, className)
     {
-        var ele = document.createElement(el);
+        const ele = document.createElement(el);
         if (id) ele.id = id;
         if (html) ele.innerHTML = html;
         if (className) ele.classList.add(className);
@@ -165,7 +191,7 @@
             toolbar.appendChild(newElement('li', 'place-holder'));
             placeHolder = view.el.querySelector('#place-holder');
         }
-        var newEle = newElement('li', label, html);
+        const newEle = newElement('li', label, html);
         placeHolder.insertAdjacentElement('afterEnd', newEle);
         return newEle;
     }
