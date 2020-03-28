ae4da380d11894de85a24b1d425018af90a14f6d
Dele Olajide <dele@4ng.net>
2020-03-28 12:19:02 +0000

    revert back to processone action

--- entrypoint.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-go get -u github.com/FluuxIO/go-xmpp
-go run /notifier/main.go "${INPUT_SERVER_HOST}" "${INPUT_RECIPIENT}" "${INPUT_JID}" "${INPUT_PASSWORD}" "${INPUT_SERVER_PORT}" "${INPUT_MESSAGE}" "${INPUT_RECIPIENT_IS_ROOM}" "${INPUT_BOT_ALIAS}"
--- go.mod
+++
@@ -1,5 +0,0 @@
-module go-xmpp_notifier
-
-go 1.13
-
-require gosrc.io/xmpp v0.4.0
