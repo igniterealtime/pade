89404d0f0c6045c39f237949e537837e612f3259
Dele Olajide <dele@4ng.net>
2022-10-21 18:46:52 +0100

    fix jitsi-meet icon

--- docs/index.js
+++ docs/index.js
@@ -1303,7 +1303,7 @@ function addControlFeatures() {
 	}
 	
 	if (!section.querySelector('.pade-meet-now')) {	
-		const ofmeetButton = newElement('a', null, '<converse-icon class="fa fa-phone right" size="1em"></converse-icon>', 'pade-meet-now', 'controlbox-heading__btn');
+		const ofmeetButton = newElement('a', null, '<svg style="width:18px; height:18px; fill:var(--subdued-color)" viewBox="0 0 32 32"><path d="M22.688 14l5.313-5.313v14.625l-5.313-5.313v4.688c0 .75-.625 1.313-1.375 1.313h-16C4.563 24 4 23.437 4 22.687V9.312c0-.75.563-1.313 1.313-1.313h16c.75 0 1.375.563 1.375 1.313V14z"></path></svg>', 'pade-meet-now', 'controlbox-heading__btn');
 		ofmeetButton.title = "Meet Now!!";
 		section.appendChild(ofmeetButton);
 
